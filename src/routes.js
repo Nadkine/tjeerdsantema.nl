7ea5974591714d13c2cfda71f52d77fb86fd236e
tjeerdsantema <t.santema@dictu.nl>
2023-11-26 13:10:19 +0100

    First commit

--- src/routes.js
+++ src/routes.js
@@ -4,72 +4,31 @@ import { Icon } from "@chakra-ui/react";
 import {
   MdBarChart,
   MdPerson,
+  MdPerson2,
   MdHome,
   MdLock,
   MdOutlineShoppingCart,
 } from "react-icons/md";
 
 // Admin Imports
-import MainDashboard from "views/admin/default";
-import NFTMarketplace from "views/admin/marketplace";
-import Profile from "views/admin/profile";
-import DataTables from "views/admin/dataTables";
-import RTL from "views/admin/rtl";
-
-// Auth Imports
-import SignInCentered from "views/auth/signIn";
+import Portfolio from "views/admin/default";
+import Personal from "views/admin/personal";
 
 const routes = [
   {
-    name: "Main Dashboard",
+    name: "Portfolio",
     layout: "/admin",
     path: "/default",
     icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
-    component: MainDashboard,
+    component: Portfolio,
   },
   {
-    name: "NFT Marketplace",
+    name: "Personal",
     layout: "/admin",
-    path: "/nft-marketplace",
-    icon: (
-      <Icon
-        as={MdOutlineShoppingCart}
-        width='20px'
-        height='20px'
-        color='inherit'
-      />
-    ),
-    component: NFTMarketplace,
-    secondary: true,
-  },
-  {
-    name: "Data Tables",
-    layout: "/admin",
-    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
-    path: "/data-tables",
-    component: DataTables,
-  },
-  {
-    name: "Profile",
-    layout: "/admin",
-    path: "/profile",
-    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
-    component: Profile,
-  },
-  {
-    name: "Sign In",
-    layout: "/auth",
-    path: "/sign-in",
-    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
-    component: SignInCentered,
-  },
-  {
-    name: "RTL Admin",
-    layout: "/rtl",
-    path: "/rtl-default",
-    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
-    component: RTL,
-  },
+    path: "/personal",
+    icon: <Icon as={MdPerson2} width='20px' height='20px' color='inherit' />,
+    component: Personal,
+  }
 ];
 
 export default routes;
--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -24,6 +24,7 @@
 import {
   Avatar,
   Box,
+  Text,
   Flex,
   FormLabel,
   Icon,
@@ -31,12 +32,6 @@ import {
   SimpleGrid,
   useColorModeValue,
 } from "@chakra-ui/react";
-// Assets
-import Usa from "assets/img/dashboards/usa.png";
-// Custom components
-import MiniCalendar from "components/calendar/MiniCalendar";
-import MiniStatistics from "components/card/MiniStatistics";
-import IconBox from "components/icons/IconBox";
 import React from "react";
 import {
   MdAddTask,
@@ -44,129 +39,90 @@ import {
   MdBarChart,
   MdFileCopy,
 } from "react-icons/md";
-import CheckTable from "views/admin/default/components/CheckTable";
-import ComplexTable from "views/admin/default/components/ComplexTable";
-import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
-import Tasks from "views/admin/default/components/Tasks";
-import TotalSpent from "views/admin/default/components/TotalSpent";
-import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
-import {
-  columnsDataCheck,
-  columnsDataComplex,
-} from "views/admin/default/variables/columnsData";
-import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
-import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
+import GithubLogo from "assets/img/GithubLogo.png";
+import GithubCard from "views/admin/default/components/GithubCard";
+import TriathlonTracker from "views/admin/default/components/TriathlonTracker";
+import SketchesCard from "views/admin/default/components/SketchesCard";
+import FullRedCard from "views/admin/default/components/FullRedCard";
+import PianoClip from 'assets/video/PianoClip.mp4';
+import PiHatClip from 'assets/video/PiHat.mp4';
+import PiPhoto from 'assets/img/OudePi.jpeg';
+
 
 export default function UserReports() {
   // Chakra Color Mode
+  const textColor = useColorModeValue("secondaryGray.900", "white");
   const brandColor = useColorModeValue("brand.500", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
-        gap='20px'
-        mb='20px'>
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />
-              }
-            />
-          }
-          name='Earnings'
-          value='$350.4'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />
-              }
-            />
-          }
-          name='Spend this month'
-          value='$642.39'
-        />
-        <MiniStatistics growth='+23%' name='Sales' value='$574.34' />
-        <MiniStatistics
-          endContent={
-            <Flex me='-16px' mt='10px'>
-              <FormLabel htmlFor='balance'>
-                <Avatar src={Usa} />
-              </FormLabel>
-              <Select
-                id='balance'
-                variant='mini'
-                mt='5px'
-                me='0px'
-                defaultValue='usd'>
-                <option value='usd'>USD</option>
-                <option value='eur'>EUR</option>
-                <option value='gba'>GBA</option>
-              </Select>
-            </Flex>
-          }
-          name='Your balance'
-          value='$1,000'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-              icon={<Icon w='28px' h='28px' as={MdAddTask} color='white' />}
-            />
-          }
-          name='New Tasks'
-          value='154'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdFileCopy} color={brandColor} />
-              }
-            />
-          }
-          name='Total Projects'
-          value='2935'
-        />
+
+      
+
+      <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='40px'>
+        
+        <SketchesCard />
+        <TriathlonTracker />
+      </SimpleGrid>
+    
+      <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>
+        <GithubCard />
+        <FullRedCard/>
+      </SimpleGrid>
+    
+      <SimpleGrid columns={{ base: 4, md: 4, xl: 4 }} gap='10px' mb='10px'>
+      <Box align='top'mt='auto'>
+          <Text
+            color={textColor}
+            fontSize='30px'
+            textAlign='center'
+            fontWeight='700'
+            lineHeight='100%'>
+            Piano + Programmeerbare ledlicht 
+          </Text>
+        
+          <div className="video-container">
+            <video autoPlay muted loop id="video">
+              <source src={PianoClip} type="video/mp4" />
+            </video>
+          </div>        
+        </Box>
+        <Box mt='auto'>
+        <Text
+            color={textColor}
+            fontSize='30px'
+            textAlign='center'
+            fontWeight='700'
+            lineHeight='100%'>
+            Kapotte Pi 
+          </Text>
+          <img src={PiPhoto}/>        
+        </Box>
+        <Box mt='auto'>
+          <Text
+            color={textColor}
+            fontSize='30px'
+            textAlign='center'
+            fontWeight='700'
+            lineHeight='100%'>
+            PiHat 
+          </Text>
+        
+          <div className="video-container">
+            <video autoPlay muted loop id="video">
+              <source src={PiHatClip} type="video/mp4" />
+            </video>
+          </div>        
+        </Box>
+        
+        <Box mt='auto'>
+          <img src={GithubLogo}/>        
+        </Box>
       </SimpleGrid>
 
-      <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>
-        <TotalSpent />
-        <WeeklyRevenue />
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
-        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-          <DailyTraffic />
-          <PieCard />
-        </SimpleGrid>
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-        <ComplexTable
-          columnsData={columnsDataComplex}
-          tableData={tableDataComplex}
-        />
-        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-          <Tasks />
-          <MiniCalendar h='100%' minW='100%' selectRange={false} />
-        </SimpleGrid>
-      </SimpleGrid>
     </Box>
   );
 }
+
+
+
